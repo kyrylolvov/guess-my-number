6bf2583e388bf31998e4abb2e1606fc496e0d0a6
Kyrylo Lvov <70720713+kyrylolvov@users.noreply.github.com>
2020-12-03 16:46:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,7 @@ document.querySelector(`.score`).textContent = `10`;
 
 console.log((document.querySelector(`.guess`).value = 23));
 */
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(`.number`).textContent = secretNumber;
-
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
 document.querySelector(`.check`).addEventListener(`click`, function () {
@@ -28,6 +26,7 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
     document.querySelector(`.message`).textContent = `Correct Number! 🎉`;
     document.querySelector(`body`).style.backgroundColor = `#0CB04A`;
     document.querySelector(`.number`).style.width = `30rem`;
+    document.querySelector(`.number`).textContent = secretNumber;
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(`.message`).textContent = `Too high! 📈`;
@@ -47,4 +46,13 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
       document.querySelector(`.score`).textContent = 0;
     }
   }
+});
+document.querySelector(`.again`).addEventListener(`click`, function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(`.score`).textContent = 20;
+  document.querySelector(`.number`).style.width = `15rem`;
+  document.querySelector(`body`).style.backgroundColor = `#222`;
+  document.querySelector(`.message`).textContent = `Start guessing...`;
+  document.querySelector(`.number`).textContent = `?`;
+  document.querySelector(`.guess`).value = ``;
 });
